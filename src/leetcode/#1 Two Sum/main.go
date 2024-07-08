e2d4914f00ad8acfb3a10f67adf619d85309e8b1
NAT1804 <167729136+Sotatek-TuanNguyen14@users.noreply.github.com>
2024-07-08 10:16:52 +0700

    feat: init project

---
+++ src/leetcode/#1 Two Sum/main.go
@@ -0,0 +1,13 @@
+package _1_Two_Sum
+
+func twoSum(nums []int, target int) []int {
+	m := make(map[int]int)
+
+	for currIdx, currNum := range nums {
+		if targetIdx, isPresent := m[target-currNum]; isPresent {
+			return []int{targetIdx, currIdx}
+		}
+		m[currNum] = currIdx
+	}
+	return []int{}
+}
